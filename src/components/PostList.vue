1fb2a6b5dd0b5aea96bd8b6b61eb14e9c100cc29
Myungseo Kang <l3opold7@gmail.com>
2018-06-14 14:46:30 +0900

    PostList 컴포넌트 추가 및 axios 를 이용해 API 서버와 연결

---
+++ src/components/PostList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="post-list">
+    <table class="post-table" border="1" v-if="hasResult">
+      <thead>
+        <tr>
+          <td>id</td>
+          <td>title</td>
+          <td>author</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <td>{{ post.id }}</td>
+          <td>{{ post.title }}</td>
+          <td>{{ post.author }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div v-else>로드중</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  created: function() {
+    const baseURI = 'http://localhost:8080';
+    this.$http.get(`${baseURI}/posts/`)
+    .then((result) => {
+      console.log(result)
+      this.posts = result.data
+    })
+  },
+  computed: {
+    hasResult: function () {
+      return this.posts.length > 0
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.post-table {
+  width: 100%;
+}
+</style>
